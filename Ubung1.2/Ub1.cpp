12f18b1cd419404d668cf306afe35a3608c88dd6
AndreiSiu <vasiu_andrei2000@yahoo.com>
2020-03-09 11:55:54 +0200

    Update Ub1.cpp

--- Ubung1.2/Ub1.cpp
+++ Ubung1.2/Ub1.cpp
@@ -1,6 +1,7 @@
-#include<iostream>
-#include "Ub1.h"
 #include "pch.h"
+#include "Ub1.h"
+#include<iostream>
+
 using namespace std;
 int Prim(int n)
 {
@@ -54,7 +55,7 @@ void schema1()
 		cout << "\nWahle eine Option: ";
 		cin >> key;
 		if (key == 1) {
-			UB1_b();
+			UB1_a();
 		}
 		if (key == 2) {
 			UB1_b();
